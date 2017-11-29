3936b22785d2c97c9108e7fba048a30412ac9b09
hyj <1075937817@qq.com>
2017-11-29 21:06:24 +0800

    首页js部分完成

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -16,12 +16,57 @@ $(function(){
         },1000);  
     });
 
+    // 二级菜单的显示与隐藏
+    $('.nav .child').prev().on('click',function(){
+        // console.log(this);
+        $(this).next().slideToggle("now");
 
+    });
 
+    // 侧边栏的显示与隐藏效果
+    $(".topBar .icon_menu").on('click',function(){
+        console.log(this);
+        $(".lt_aside").toggleClass("now");
+        $(".lt_main").toggleClass("now");
 
+    });
 
+    // 退出
+    $(".topBar .icon_logout").on('click',function(){
+        console.log(this);
+        $('#myModal').modal('show');
 
-    
+        //因为jquery注册事件不会覆盖。
+        //off()解绑所有的事件
+        //off("click")
+        $(".btn_logout").off().on('click',function(){
+            //发送ajax请求，告诉服务器，需要退出
+            $.ajax({
+                type:'get',
+                url:"/employee/employeeLogout",
+                success:function(data){
+                    // console.log(data);
+                    if(data.success){
+                        location.href="login.html";
+                    }
+                }
+            });
+        });
+    });
+// ============实现退出   方法2================
+    // $(".btn_logout").on('click',function(){
+    //     $.ajax({
+    //         type:'get',
+    //         url:"/employee/employeeLogout",
+    //         success:function(data){
+    //             // console.log(data);
+    //             if(data.success){
+    //                 location.href="login.html";
+    //             }
+    //         }
+    //     });
+    // });
+
 });
 
 
